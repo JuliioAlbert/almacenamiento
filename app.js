956e146230a427f4d2acec56f8631667c629af6e
JULIO <juliioalbert98@gmail.com>
2020-10-26 21:40:24 -0600

    deploy 4

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const conectarDB = require("./config/db");
-require("cors");
+const cors = require("cors");
 const bodyParser = require('body-parser');
 //crear el Servidor
 const app = express();
 
-//Acceso CORS
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-  next();
-});
+//Habilitar Cors
+app.use(cors());
 
 //Conectar a la base de datos
 conectarDB();

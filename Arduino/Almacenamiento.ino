ce828027a43932ad8c3a6c1f5e697e226287ecef
JULIO <juliioalbert98@gmail.com>
2020-11-03 23:21:22 -0600

    Final

--- Arduino/Almacenamiento.ino
+++ Arduino/Almacenamiento.ino
@@ -2,14 +2,17 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-int verde = D8;  // LED connected to digital pin 13
-int rojo = D6;  // LED connected to digital pin 13
-int inPin = D5;    
-int inPin2 = D4;
-int val = 0;
-int val2 = 0;      
+int verde = D7;  // LED connected to digital pin 13
+int rojo = D8;  // LED connected to digital pin 13
+int izq = D1;    
+int der = D2;
+int d = 0;
+int i = 0;      
 
 int ultimo = 0;
+int a= 0;
+int b=0;
+
 
 //Server
 String server = "http://almacenamiento.herokuapp.com/api/sensor";
@@ -20,8 +23,8 @@ void setup() {
   Serial.begin(115200);    //iniciar puerto serie
   pinMode(verde, OUTPUT); 
   pinMode(rojo, OUTPUT); 
-  pinMode(inPin, INPUT);    //In
-  pinMode(inPin2, INPUT);    //In
+  pinMode(der, INPUT);    //In
+  pinMode(izq, INPUT);    //In
 
   //Conexion a la Red Wifi
    WiFi.begin("julio", "huevos2020");
@@ -41,17 +44,33 @@ void setup() {
 
 void loop() {
   
-  val = digitalRead(inPin);   // 
-  val2 = digitalRead(inPin2);   // 
-  if(val== 1 && val2== 0 && ultimo == 0) {
-     ultimo =1; 
-     encender();
-     post("true");
-  }else if(val == 0 && val2 ==1 && ultimo ==1 ){
-    ultimo =0;
+  d = digitalRead(der);   // 
+  i = digitalRead(izq);   //
+  Serial.println(d); 
+  delay(500);
+  Serial.println(i); 
+
+  if(d == 0 && i == 0 && ultimo == 0){
+    //post("false");
     apagado();
-    post("false");
+    Serial.println("Apagado");
+    b=1;
+  }else if(d == 1 && i == 0 && ultimo == 0 && b==1){
+    //post("true");
+    encender();
+    Serial.println("ED");
+    a=1;
+    b=0;
+    ultimo=0;
+  }else if(d == 0 && i == 1 && a==1 ){
+    //post("true");
+    Serial.println("EI");
+    encender();
+    b=1;
+    a=0;
+    ultimo=0;
   }
+  
 }
   
   void post(String estado) {
